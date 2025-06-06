03691f0424b16dfc4395fd0faed59238aac1e823
Dinh Quoc Han <dqh@dinhquochan.com>
2025-06-06 18:03:04 +0700

    improve: SePayClient::class

--- src/SePayClient.php
+++ src/SePayClient.php
@@ -76,7 +76,9 @@ class SePayClient
     public function request(string $method, string $url, array $data = []): array
     {
         try {
-            $response = Http::baseUrl('https://my.sepay.vn/api/v1')
+            $baseUrl = SEPAY_FOB_URL . '/api/v1';
+
+            $response = Http::baseUrl($baseUrl)
                 ->withToken(setting()->get('sepay_access_token'))
                 ->$method($url, $data);
 
@@ -84,7 +86,7 @@ class SePayClient
                 try {
                     $this->refreshToken();
 
-                    $response = Http::baseUrl('https://my.sepay.vn/api/v1')
+                    $response = Http::baseUrl($baseUrl)
                         ->withToken(setting()->get('sepay_access_token'))
                         ->$method($url, $data);
                 } catch (Exception $e) {
